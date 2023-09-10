027f4cbd8368089a9b29cf8731a5572c9faad7e3
Pritam patidar <pritampatidar29@gmail.com>
2023-09-10 23:17:13 +0530

    OnThisDayComponentAdd

--- src/app/Service/Service.ts
+++ src/app/Service/Service.ts
@@ -11,11 +11,19 @@ constructor(private _http:HttpClient) { }
 getBirthData(currentDate:Date){
               let day =  currentDate.getDate();
               let month = currentDate.getMonth() + 1;              
-              console.log(day);
-              console.log(month)
-             //return this._http.get('https://mocki.io/v1/6237ceb7-e2a6-4410-9de0-bd94ed69e502');
-             return this._http.get('https://en.wikipedia.org/api/rest_v1/feed/onthisday/births/'+month +'/'+ day);
+             // console.log(day);
+              //console.log(month)
+              return this._http.get('https://en.wikipedia.org/api/rest_v1/feed/onthisday/births/'+month +'/'+ day);
             
         }
+
+getDeathData(currentDate:Date){
+            let day =  currentDate.getDate();
+            let month = currentDate.getMonth() + 1;              
+            return this._http.get('https://en.wikipedia.org/api/rest_v1/feed/onthisday/deaths/'+month +'/'+ day);
+          
+      }     
+    
+
  }
 
--- src/app/components/on-this-day/on-this-day.component.ts
+++ src/app/components/on-this-day/on-this-day.component.ts
@@ -6,34 +6,63 @@ import { ApiserviceService } from 'src/app/Service/Service';
   styleUrls: ['./on-this-day.component.css']
 })
 export class OnThisDayComponent {
-  //newdata:any;
   data:any;
   displayData:Array<any>;
+  displayDeathData:Array<any>;
   constructor(private _apiservice:ApiserviceService){ 
     this.displayData=[];
+    this.displayDeathData=[];
   }
+
   ngOnInit(){
-    let currentDate=new Date("10-09-2023");
+    this.getBirthData();
+    this.getDeathData();
+   }
+  getBirthData(){
+    //console.log("get birth data");
+    let currentDate=new Date();
     this._apiservice.getBirthData(currentDate).subscribe(res=>{
     this.data=res;
     for(let i=0;i<2;i++){
       let personVo = {
         name:String,
         description:String,
-        thumbnail:String   //change
+        thumbnail:String   
       }
       personVo.name = this.data.births[i].pages[0].title;
-      personVo.thumbnail=this.data.births[i].pages[0].thumbnail ?.source; //change 
+      personVo.thumbnail=this.data.births[i].pages[0].thumbnail ?.source; 
       personVo.description = this.data.births[i].pages[0].description;
       
-      this.displayData.push(personVo);
+       this.displayData.push(personVo);
    }
       this.displayData;
-      console.log(this.displayData);
-      console.log(this.data.births);
-      
-     })
+      // console.log(this.displayData);
+      // console.log(this.data.births);
+      // console.log(currentDate);
+     
+    })
   }
+  getDeathData(){
+   // console.log("get Death data");
+    let currentDate=new Date();
+    this._apiservice.getDeathData(currentDate).subscribe(res=>{
+    this.data=res;
+    for(let i=0;i<2;i++){
+      let personVo = {
+        name:String,
+        description:String,
+        thumbnail:String   
+      }
+      personVo.name = this.data.deaths[i].pages[0].title;
+      personVo.thumbnail=this.data.deaths[i].pages[0].thumbnail ?.source;  
+      personVo.description = this.data.deaths[i].pages[0].description;
+      
+       this.displayDeathData.push(personVo);
+   }
+      this.displayDeathData;
+  })
+  }
+      
 }
 
 
