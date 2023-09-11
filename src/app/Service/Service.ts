4e6da77d50ab6bfae7b5892e8c391b16837d6b7c
Pritam patidar <pritampatidar29@gmail.com>
2023-09-11 12:12:02 +0530

    AnnouncementComponentAdd

--- src/app/Service/Service.ts
+++ src/app/Service/Service.ts
@@ -8,6 +8,7 @@ import { Injectable } from "@angular/core";
 export class ApiserviceService {
 constructor(private _http:HttpClient) { }
 
+// for oneThisDay
 getBirthData(currentDate:Date){
               let day =  currentDate.getDate();
               let month = currentDate.getMonth() + 1;              
@@ -16,7 +17,6 @@ getBirthData(currentDate:Date){
               return this._http.get('https://en.wikipedia.org/api/rest_v1/feed/onthisday/births/'+month +'/'+ day);
             
         }
-
 getDeathData(currentDate:Date){
             let day =  currentDate.getDate();
             let month = currentDate.getMonth() + 1;              
@@ -24,6 +24,12 @@ getDeathData(currentDate:Date){
           
       }     
     
+// for announcement
+ getAnnouncement(){
+   
+      return this._http.get('https://en.wikipedia.org/api/rest_v1/feed/announcements')
+ }
+
 
  }
 
--- src/app/components/announcement/announcement.component.ts
+++ src/app/components/announcement/announcement.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { ApiserviceService } from 'src/app/Service/Service';
 @Component({
   selector: 'app-announcement',
   templateUrl: './announcement.component.html',
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AnnouncementComponent {
 
+  data:any;
+  displayData:Array<any>
+  constructor(private _apiservice:ApiserviceService){ 
+  this.displayData=[];
+  }
+
+    ngOnInit(){
+     this.getAnnouncement();
+    }
+    getAnnouncement(){
+    //console.log("get Announcement data");
+    
+     this._apiservice.getAnnouncement().subscribe(res=>{
+     this.data=res;
+ //    console.log(res);
+     for(let i=0;i<this.data.announce.length;i++){
+      let AnnouncementVo = {
+        text:String,
+      }
+      AnnouncementVo.text = this.data.announce[i].text;
+      this.displayData.push(AnnouncementVo);
+   }
+       this.displayData;
+      // console.log(this.displayData);
+      })
+  }
+  
 }
